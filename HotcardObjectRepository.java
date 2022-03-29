ab14215db6813742c3a5a79e2c5174fa393f31ea
sadichchha7 <61775054+sadichchha7@users.noreply.github.com>
2022-03-29 10:43:38 +0530

    Add files via upload

---
+++ HotcardObjectRepository.java
@@ -0,0 +1,31 @@
+package objectRepository;
+
+public class HotcardObjectRepository {
+	public String login_tb_username = "//*[@id=\"username\"]";
+	public String login_tb_password = "//*[@id=\"password\"]";
+	public String login_bt_login = "//*[@id=\"signOnButton\"]";
+	public String home_page_bt_tools = "//*[@id=\"nav_tools\"]";
+	public String bt_back = "//*[@id=\"goBack\"]";
+	public String institution = "//*[@id=\"inst\"]";
+	public String search_for_text = "//*[@id=\"searchText\"]";
+	public String search_type_global = "//*[@id=\"searchType1\"]";
+	public String search_type_surname = "//*[@id=\"searchType2\"]";
+	public String bt_search = "//*[@id=\"searchBean\"]/table/tbody/tr[5]/td[1]/input";
+	public String result = "/html/body/table/thead/tr[1]/td[1]";
+	public String tx_institution = "/html/body/table/thead/tr[1]/td[2]";
+	public String tx_search_text = "/html/body/table/thead/tr[1]/td[3]";
+	public String name = "/html/body/table/thead/tr[2]/td[1]";
+	public String address = "/html/body/table/thead/tr[2]/td[2]";
+	public String dob = "/html/body/table/thead/tr[2]/td[3]";
+	public String card_number = "/html/body/table/thead/tr[2]/td[4]";
+	public String masked_card = "//*[@id=\"viewPanBtn0\"]";
+	public String status = "/html/body/table/thead/tr[2]/td[6]";
+	public String card_number_1 = "//*[@id=\"viewPanBtn0\"]";
+	public String hotcard_link = "/html/body/table/tbody/tr[3]/td/table/tbody/tr/td[1]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/p/a";
+	public String home_page_bt_hotcard_search = "/html/body/ul[3]/li[9]/a";
+	public String logout = "/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr/td[6]/a/img";
+	public String error_message = "//*[@id=\"searchBean\"]/table/tbody/tr[6]/td/p";
+	public String scroll = "/html/body/table/tbody/tr[212]/td[2]";
+	public String scroll1 = "/html/body/table/tbody/tr[21]/td[2]";
+
+}
